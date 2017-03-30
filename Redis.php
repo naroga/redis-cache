fc43a0c3eb1616ea044e2c0b20dbf5e007f21538
Derek Stephen McLean <delboy1978uk@gmail.com>
2017-03-30 13:10:49 +0200

    Update Redis.php

--- Redis.php
+++ Redis.php
@@ -8,6 +8,7 @@ use Predis\Client;
 use Predis\ClientInterface;
 use Psr\SimpleCache\CacheInterface;
 use Psr\SimpleCache\DateInterval;
+use Traversable;
 
 /**
  * Class Redis
@@ -101,11 +102,11 @@ class Redis implements CacheInterface
     /** @inheritDoc */
     public function getMultiple($keys, $default = null)
     {
-        if (!is_array($keys) && !$keys instanceof \Traversable) {
+        if (!is_array($keys) && !$keys instanceof Traversable) {
             throw new InvalidArgumentException("Keys must be an array or a \\Traversable instance.");
         }
 
-        $result = [];
+        $result = array();
         foreach ($keys as $key) {
             $result[$key] = $this->get($key, $default);
         }
@@ -116,7 +117,7 @@ class Redis implements CacheInterface
     /** @inheritDoc */
     public function setMultiple($values, $ttl = null)
     {
-        if (!is_array($values) && !$values instanceof \Traversable) {
+        if (!is_array($values) && !$values instanceof Traversable) {
             throw new InvalidArgumentException("Values must be an array or a \\Traversable instance.");
         }
 
@@ -138,7 +139,7 @@ class Redis implements CacheInterface
     /** @inheritDoc */
     public function deleteMultiple($keys)
     {
-        if (!is_array($keys) && !$keys instanceof \Traversable) {
+        if (!is_array($keys) && !$keys instanceof Traversable) {
             throw new InvalidArgumentException("Keys must be an array or a \\Traversable instance.");
         }
 
